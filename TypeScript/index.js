7f28c200e5ca20e776fa991aad792102f5d35acd
Amruth <ammumandappa@gmail.com>
2024-06-12 22:18:32 +0530

    compiling TypeScript folder
    npm ts file name,
    it helps us to create a js file same as ts to run the program.

---
+++ TypeScript/index.js
@@ -0,0 +1,4 @@
+console.log("TypeScript tutorial");
+var num = 5;
+ num = "qwerty"
+console.log(num);
---
+++ TypeScript/index.ts
@@ -0,0 +1,6 @@
+ console.log("TypeScript tutorial")
+
+ let num:number = 5;
+//  num = "qwerty"
+
+ console.log(num)
