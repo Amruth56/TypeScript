8757d4a5f300b4290a39660ffda3af32a7fef256
Amruth <ammumandappa@gmail.com>
2024-06-14 20:22:55 +0530

    array operations

---
+++ TypeScript/04arrays.js
@@ -0,0 +1,7 @@
+"use strict";
+const array = [1, 2, 3, 4, 5];
+const arr2 = new Array(1, 3, 5, 7, 9);
+const arr3 = Array.of("qwe", "rty", "mno", "xyz");
+console.log(array);
+console.log(arr2);
+console.log(arr3);
---
+++ TypeScript/04arrays.ts
@@ -0,0 +1,10 @@
+const array:number[] = [1,2,3,4,5  ]
+
+const arr2:number[] = new Array(1,3,5,7,9)
+
+const arr3:string[] = Array.of("qwe", "rty", "mno", "xyz")
+
+
+console.log(array)
+console.log(arr2)
+console.log(arr3)
