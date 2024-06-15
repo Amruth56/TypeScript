23a488a8829a1dcf940f98e2bf7460b5588eb1f0
Amruth <ammumandappa@gmail.com>
2024-06-15 18:41:26 +0530

    type Aliases in typescript

---
+++ TypeScript/07typeAliases.js
@@ -0,0 +1,47 @@
+"use strict";
+/* In TypeScript, a type is a way to give a name to a specific type or combination of types. It allows you to create a custom name for qa type, making it easier to reuse and refer to the same type indifferent parts of your code */
+const person1 = {
+    name: "John",
+    age: 21,
+    isStudent: true,
+    address: {
+        city: "Bengaluru",
+        state: "Karnataka",
+        country: "India",
+    },
+};
+const person2 = {
+    name: "John",
+    age: 21,
+    isStudent: true,
+    address: {
+        city: "Bengaluru",
+        state: "Karnataka",
+        country: "India",
+    },
+};
+const person3 = {
+    name: "John",
+    age: 21,
+    isStudent: true,
+    address: {
+        city: "Bengaluru",
+        state: "Karnataka",
+        country: "India",
+    },
+};
+const person4 = {
+    name: "John",
+    age: 21,
+    isStudent: true,
+    address: {
+        city: "Bengaluru",
+        state: "Karnataka",
+        country: "India",
+    },
+};
+//accessing an object data
+console.log(person1.address.state);
+//updating object properties
+person1.address.state = "usa";
+console.log(person1.address.state);
---
+++ TypeScript/07typeAliases.ts
@@ -0,0 +1,63 @@
+/* In TypeScript, a type is a way to give a name to a specific type or combination of types. It allows you to create a custom name for qa type, making it easier to reuse and refer to the same type indifferent parts of your code */
+
+
+type Person = {
+    name: string;
+    age: number;
+    isStudent: boolean;
+    address: {
+      city: string;
+      state: string;
+      country: string;
+    };
+}
+
+const person1:Person = {
+    name: "John",
+    age: 21,
+    isStudent: true,
+    address: {
+      city: "Bengaluru",
+      state: "Karnataka",
+      country: "India",
+    },
+  };
+
+const person2:Person = {
+    name: "John",
+    age: 21,
+    isStudent: true,
+    address: {
+      city: "Bengaluru",
+      state: "Karnataka",
+      country: "India",
+    },
+  };
+const person3:Person = {
+    name: "John",
+    age: 21,
+    isStudent: true,
+    address: {
+      city: "Bengaluru",
+      state: "Karnataka",
+      country: "India",
+    },
+  };
+const person4:Person = {
+    name: "John",
+    age: 21,
+    isStudent: true,
+    address: {
+      city: "Bengaluru",
+      state: "Karnataka",
+      country: "India",
+    },
+  };
+  
+  //accessing an object data
+  console.log(person1.address.state);
+  
+  //updating object properties
+  person1.address.state = "usa";
+  console.log(person1.address.state);
+  
