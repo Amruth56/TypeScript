0424601b7c9b1d42619276f6cf00889d27d17064
Amruth <ammumandappa@gmail.com>
2024-06-17 14:57:25 +0530

    union and intersection homework

--- TypeScript/11union_intersection.js
+++ TypeScript/11union_intersection.js
@@ -1,20 +1,37 @@
 "use strict";
 // when using a union type, it is essential to ensure that at least one of the types in the union includes all the required properties, Failure to do so will result in a type error during compilation.
-// const inputValue = (value:string | number | boolean):void =>{
-// }
-// inputValue(value:55)
-// inputValue(value:'jbvgcg')
-// inputValue(value:true)
 const userInput = (value) => {
-    if (typeof value === 'string') {
+    if (typeof value === "string") {
         return value.toUpperCase();
     }
-    else if (typeof value === 'number') {
+    else if (typeof value === "number") {
         return value * 2;
     }
     else {
-        throw new Error('Invalid input data');
+        throw new Error("Invalid input data");
     }
 };
-console.log(userInput('hello'));
+console.log(userInput("hello"));
 console.log(userInput(10));
+// homework
+/* we want ot create a function that formats the value passed to it in a specific way based on its type:
+-> if the input is a number, it should add a dollar sign and format it with two decimal places.
+-> if the input is a boolean, it should return "yes" for true and "No" for false
+->if the input is a string, it should capitalize the first letter.*/
+function format(v) {
+    if (typeof v === "number") {
+        return `$${v.toFixed(2)}`;
+    }
+    else if (typeof v === "boolean") {
+        return v ? "Yes" : "No";
+    }
+    else if (typeof v === "string") {
+        return v.charAt(0).toUpperCase() + v.slice(1);
+    }
+    else {
+        throw new Error("Invalid value");
+    }
+}
+console.log(format(1234672));
+console.log(format("hello"));
+console.log(format(true));
--- TypeScript/11union_intersection.ts
+++ TypeScript/11union_intersection.ts
@@ -1,17 +1,36 @@
 // when using a union type, it is essential to ensure that at least one of the types in the union includes all the required properties, Failure to do so will result in a type error during compilation.
 
+const userInput = (value: string | number): string | number => {
+  if (typeof value === "string") {
+    return value.toUpperCase();
+  } else if (typeof value === "number") {
+    return value * 2;
+  } else {
+    throw new Error("Invalid input data");
+  }
+};
 
+console.log(userInput("hello"));
+console.log(userInput(10));
 
+// homework
+/* we want ot create a function that formats the value passed to it in a specific way based on its type:
+-> if the input is a number, it should add a dollar sign and format it with two decimal places.
+-> if the input is a boolean, it should return "yes" for true and "No" for false 
+->if the input is a string, it should capitalize the first letter.*/
 
-const userInput = (value : string | number ) : string | number => {
-    if(typeof value === 'string'){
-        return value.toUpperCase()
-    } else if(typeof value === 'number'){
-        return value*2;
-    }else {
-        throw new Error('Invalid input data')
-    }
+function format(v: string | number | boolean): string | number | boolean {
+  if (typeof v === "number") {
+    return `$${v.toFixed(2)}`;
+  } else if (typeof v === "boolean") {
+    return v ? "Yes" : "No";
+  } else if (typeof v === "string") {
+    return v.charAt(0).toUpperCase() + v.slice(1);
+  } else {
+    throw new Error("Invalid value");
+  }
 }
 
-console.log(userInput( 'hello'));
-console.log(userInput( 10));
+console.log(format(123.42));
+console.log(format("hello"));
+console.log(format(true));
