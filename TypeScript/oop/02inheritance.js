314d2c3be41a4b0b824f1b20f97177f285863a33
Amruth <ammumandappa@gmail.com>
2024-06-18 18:37:32 +0530

    update inheritance

--- TypeScript/oop/02inheritance.js
+++ TypeScript/oop/02inheritance.js
@@ -1,12 +1,4 @@
 "use strict";
-class Per {
-    name = "John";
-    age = 30;
-    hobbies = ['reading', 'anime', 'painting'];
-}
-const pe = new Persons();
-console.log(pe);
-console.log(pe.age);
 class Stu {
     name;
     age;
@@ -16,9 +8,26 @@ class Stu {
         this.age = age;
         this.hobbies = hobbies;
     }
+    introduce() {
+        console.log(`My name is ${this.name}. I am ${this.age} years old. I love ${this.hobbies.join(',')}.`);
+    }
+}
+//Inheritance 
+class Chil extends Stu {
+    grade;
+    constructor(name, age, hobbies, grade) {
+        super(name, age, hobbies);
+        this.grade = grade;
+    }
+    introduce() {
+        console.log(`My name is ${this.name}. I am ${this.age} years old. I love ${this.hobbies.join(',')} and I am in grade ${this.grade}.`);
+    }
 }
 const stu1 = new Stu("vinod", 21, ['reading', 'writing']);
 const stu2 = new Stu("Akash", 27, ['coding', 'gym']);
-console.log(stu1.name, stu2.name);
-console.log(stu1);
-console.log(stu2);
+const chil1 = new Chil("zoro", 27, ['playing', 'dancing'], 5);
+// console.log(stu1.name, stu2.name);
+// console.log(stu1);
+// console.log(stu2);
+// console.log(stu2.introduce());
+console.log(chil1.introduce());
--- TypeScript/oop/02inheritance.ts
+++ TypeScript/oop/02inheritance.ts
@@ -1,13 +1,3 @@
-class Per {
-    name: string = "John"
-    age: number = 30
-    hobbies: string[] = ['reading', 'anime', 'painting']
-}
-
-const pe : Persons = new Persons();
-console.log(pe)
-console.log(pe.age)
-
 
 
 
@@ -21,11 +11,33 @@ class Stu{
         this.age = age;
         this.hobbies =hobbies;
     }  
+
+    introduce():void{
+        console.log(`My name is ${this.name}. I am ${this.age} years old. I love ${this.hobbies.join( ',')}.`)
+    }
+}
+
+//Inheritance 
+class Chil extends Stu{
+    grade:number;
+    constructor(name:string, age:number, hobbies:string[],grade:number)
+    {
+        super(name, age,hobbies)
+        this.grade = grade;
+    }
+    introduce():void{
+        console.log(`My name is ${this.name}. I am ${this.age} years old. I love ${this.hobbies.join( ',')} and I am in grade ${this.grade}.`)
+    }
 }
 
 const stu1: Stu = new Stu("vinod", 21, ['reading', 'writing']);
 const stu2: Stu = new Stu("Akash", 27, ['coding', 'gym'])
+const chil1: Stu = new Chil("zoro", 27, ['playing', 'dancing'], 5)
 
 console.log(stu1.name, stu2.name)
 console.log(stu1)
 console.log(stu2)
+console.log(stu2.introduce());
+console.log(chil1.introduce());
+
+
