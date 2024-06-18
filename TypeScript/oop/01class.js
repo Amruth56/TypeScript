d92d0bda345ab0cc17e84aba7610dbf3dd960419
Amruth <ammumandappa@gmail.com>
2024-06-18 16:13:30 +0530

    class in ts

---
+++ TypeScript/oop/01class.js
@@ -0,0 +1,8 @@
+"use strict";
+class Persons {
+    name = "John";
+    age = 30;
+    hobbies = ['reading', 'anime', 'painting'];
+}
+const p = new Persons();
+console.log(p);
---
+++ TypeScript/oop/01class.ts
@@ -0,0 +1,8 @@
+class Persons {
+    name: string = "John"
+    age: number = 30
+    hobbies: string[] = ['reading', 'anime', 'painting']
+}
+
+const p : Persons = new Persons();
+console.log(p)
