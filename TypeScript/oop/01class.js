2b45f35ea6c4a19bbec845c7f43791ee28ad3436
Amruth <ammumandappa@gmail.com>
2024-06-18 16:25:25 +0530

    pass value as arguments

--- TypeScript/oop/01class.js
+++ TypeScript/oop/01class.js
@@ -6,3 +6,22 @@ class Persons {
 }
 const p = new Persons();
 console.log(p);
+console.log(p.age);
+
+
+
+class Std {
+    name;
+    age;
+    hobbies;
+    constructor(name, age, hobbies) {
+        this.name = name;
+        this.age = age;
+        this.hobbies = hobbies;
+    }
+}
+const std1 = new Std("vinod", 21, ['reading', 'writing']);
+const std2 = new Std("Akash", 27, ['coding', 'gym']);
+console.log(std1.name, std2.name);
+console.log(std1);
+console.log(std2);
--- TypeScript/oop/01class.ts
+++ TypeScript/oop/01class.ts
@@ -6,3 +6,26 @@ class Persons {
 
 const p : Persons = new Persons();
 console.log(p)
+console.log(p.age)
+
+
+
+
+class Std{
+    name:string;
+    age:number;
+    hobbies:string[];
+
+    constructor(name:string, age:number, hobbies:string[]){
+        this.name = name;
+        this.age = age;
+        this.hobbies =hobbies;
+    }
+}
+
+const std1: Std = new Std("vinod", 21, ['reading', 'writing']);
+const std2: Std = new Std("Akash", 27, ['coding', 'gym'])
+
+console.log(std1.name, std2.name)
+console.log(std1)
+console.log(std2)
