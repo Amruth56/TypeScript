4629ca44c2c318eb0bbd46cb2244c761620f0e25
Amruth <ammumandappa@gmail.com>
2024-06-20 09:04:45 +0530

    interface use

--- TypeScript/oop/07typesVSinterface.js
+++ TypeScript/oop/07typesVSinterface.js
@@ -4,16 +4,24 @@
 // interfaces can extend other interfaces to inherit their members
 // interfaces can also be used to define function types
 // custom types can use unions and intersections for more complex type composition.
+;
+;
 const bioData = {
     name: "John Doe",
     age: 36,
 };
 console.log(bioData);
 console.log(bioData.age);
+const bioData1 = {
+    name: "John Doe",
+    age: 36,
+    city: "Washington",
+};
 const bioData2 = {
     name: "John Doe",
     age: 36,
     city: "Washington",
     state: "Washington"
 };
+console.log(bioData1);
 console.log(bioData2);
--- TypeScript/oop/07typesVSinterface.ts
+++ TypeScript/oop/07typesVSinterface.ts
@@ -4,6 +4,10 @@
 // interfaces can also be used to define function types
 // custom types can use unions and intersections for more complex type composition.
 
+
+
+// TYPE
+/*
 type Stud = {
   name: string;
   age: number;
@@ -21,11 +25,58 @@ const bioData: Stud = {
 console.log(bioData);
 console.log(bioData.age);
 
+
+type bioDataInter = Stud | StudAddr
 const bioData2: Stud & StudAddr = {
     name: "John Doe",
     age: 36,
     city: "Washington",
     state: "Washington"
 }
+const bioData2: bioDataInter = {
+    name: "John Doe",
+    age: 36,
+    city: "Washington",
+}
+console.log(bioData2);
+
+*/
+
+
+// INTERFACE
+
+interface Stud  {
+  name: string;
+  age: number;
+};
+
+interface StudAddr  {
+  city: string;
+  state: string;
+};
+
+const bioData: Stud = {
+  name: "John Doe",
+  age: 36,
+};
+console.log(bioData);
+console.log(bioData.age);
+
+
+
+interface bioDataInter extends Stud, StudAddr{}
+const bioData1: Stud | StudAddr = {
+    name: "John Doe",
+    age: 36,
+    city: "Washington",
+}
+const bioData2: bioDataInter = {
+    name: "John Doe",
+    age: 36,
+    city: "Washington",
+    state: "Washington"
+
+}
+console.log(bioData1);
 
 console.log(bioData2);
